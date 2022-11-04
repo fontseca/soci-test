b908000f64417d243016917d48111f3834ea9c24
Jeremy Fonseca <fonseca.dev@outlook.com>
2022-11-04 13:47:46 -0600

    create dir on makefile

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 soci-test:
+	mkdir bin
 	g++ -o bin/soci-test src/soci-test.cpp -lsoci_core -lsoci_sqlite3
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 sudo apt install libsoci-dev libsoci-sqlite3-4.0 libsoci-sqlite3-4.0-dbgsym
-
-mkdir ./bin
